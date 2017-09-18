28b8fb51c1eddea2a9c59811d74bb60abbdc5fa1
Spence Peacock <spencerpeacock@gmail.com>
2017-09-18 18:10:31 -0500

    integrated port

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+$(document).ready(function(){
+    rollLoad();
+    wobble();
+    cartoonMe();
+})
+
+
+
 $(function() {
   $('.shit').on('click', function(e) {
     e.preventDefault();
@@ -5,3 +13,28 @@ $(function() {
   });
 });    
 
+function rollLoad(){
+    $(".shp").textillate({
+      in:{ delay: 200,effect: 'rollIn', reverse:true},
+      out: {effect: 'pulse', sequence: true}
+   });
+}
+
+function wobble(){
+    $('.ripple').on('hover', function(){
+        $(this).textillate({in: {effect: 'wobble'}});
+    })
+}
+
+function cartoonMe(){
+    $(".me").click(function(){
+        var get = $(this).attr('other');
+        var cat = $(this).attr('src');
+        $(this).attr('src',get);
+        $(this).attr('other',cat);
+    })
+}
+
+  
+  
+  
